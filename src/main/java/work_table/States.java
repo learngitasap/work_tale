b91d2e9e2939f020a542d2ddfa7050e08e714bfe
DP <ajaystudynow@gmail.com>
2021-03-07 22:44:10 +0530

    merge2b

--- src/main/java/work_table/States.java
+++ src/main/java/work_table/States.java
@@ -7,7 +7,6 @@ public class States {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//this is to try merging and resolve its conflicts
-		// A new branch is being created for this and merge with another branch
-		//conflicts needs to be resolved
+		//new branch is being created for this
 }
 }
